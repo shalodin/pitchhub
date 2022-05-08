1e7f3bb9d672bde42fba5b713c78856271750b08
shalodin <shalodin.chepkoech@student.moringaschool.com>
2022-05-08 15:17:51 +0300

    added requirements.txt file

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("tonuikipkorir1@gmail.com")
+    MAIL_USERNAME = os.environ.get("shalodinchepkoech@gmail.com")
     MAIL_PASSWORD = os.environ.get("password")
 
     # simple mde  configurations
